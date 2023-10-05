bb8a2b24deca9e69cb3452e9090aba7d4c88db1c
Antoine Falisse <antoinefalisse@gmail.com>
2023-10-05 15:46:23 -0700

    fixing index

--- gait_analysis/function/handler.py
+++ gait_analysis/function/handler.py
@@ -147,7 +147,7 @@ def handler(event, context):
     # Datasets
     colNames = gait[last_leg].coordinateValues.columns
     data = gait[last_leg].coordinateValues.to_numpy()
-    coordValues = data[indices['start']:indices['end']]
+    coordValues = data[indices['start']:indices['end']+1]
     datasets = []
     for i in range(coordValues.shape[0]):
         datasets.append({})
